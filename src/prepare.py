6eca94ee19fae6bacf2e33533b05c6240495ac07
Prlfma <kordan.pavlo@gmail.com>
2026-03-30 23:33:07 +0300

    Add code formatting with black and linting with flake8

--- src/prepare.py
+++ src/prepare.py
@@ -5,17 +5,30 @@ import os
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
 
-def main():
-    parser = argparse.ArgumentParser(description="Prepare a Music Popularity data for Prediction model.")
 
-    parser.add_argument("--data_input", type=str, default="data/raw/dataset.csv", help="Path to the raw dataset")
-    parser.add_argument("--data_output", type=str, default="data/prepared/", help="Path to the save dict")
+def main():
+    parser = argparse.ArgumentParser(
+        description="Prepare a Music Popularity data for Prediction model."
+    )
+
+    parser.add_argument(
+        "--data_input",
+        type=str,
+        default="data/raw/dataset.csv",
+        help="Path to the raw dataset",
+    )
+    parser.add_argument(
+        "--data_output",
+        type=str,
+        default="data/prepared/",
+        help="Path to the save dict",
+    )
 
     args = parser.parse_args()
 
     df = pd.read_csv(args.data_input)
 
-    df.dropna(subset=['artists'], inplace=True)
+    df.dropna(subset=["artists"], inplace=True)
     df = df[df.popularity != 0].copy()
 
     drop_cols = ["index", "track_id", "track_name", "album_name", "artists"]
@@ -23,18 +36,19 @@ def main():
 
     df["explicit"] = df["explicit"].astype(int)
 
-    encoder = OneHotEncoder(sparse_output=False, handle_unknown='ignore')
+    encoder = OneHotEncoder(sparse_output=False, handle_unknown="ignore")
     encoded_genre = encoder.fit_transform(df[["track_genre"]])
     genre_columns = encoder.get_feature_names_out(["track_genre"])
     genre_df = pd.DataFrame(encoded_genre, columns=genre_columns, index=df.index)
 
     df_encoded = pd.concat([df.drop("track_genre", axis=1), genre_df], axis=1)
 
-    df_train, df_test= train_test_split(df_encoded, test_size=0.2, random_state=42)
+    df_train, df_test = train_test_split(df_encoded, test_size=0.2, random_state=42)
     os.makedirs(args.data_output, exist_ok=True)
 
     df_train.to_csv(args.data_output + "train.csv", index=False)
     df_test.to_csv(args.data_output + "test.csv", index=False)
+
 
 if __name__ == "__main__":
     main()
--- src/train.py
+++ src/train.py
@@ -10,13 +10,25 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 import json
 
+
 def main():
-    parser = argparse.ArgumentParser(description="Train a Music Popularity Prediction model.")
+    parser = argparse.ArgumentParser(
+        description="Train a Music Popularity Prediction model."
+    )
     parser.add_argument("--n_estimators", type=int, default=145, help="Number of trees")
     parser.add_argument("--max_depth", type=int, default=20, help="Maximum depth")
-    parser.add_argument("--min_samples_split", type=int, default=9, help="Min samples split")
-    parser.add_argument("--data_path", type=str, default="data/prepared/", help="Path to the train dataset")
-    parser.add_argument("--model_path", type=str, default="models/", help="Path to save model")
+    parser.add_argument(
+        "--min_samples_split", type=int, default=9, help="Min samples split"
+    )
+    parser.add_argument(
+        "--data_path",
+        type=str,
+        default="data/prepared/",
+        help="Path to the train dataset",
+    )
+    parser.add_argument(
+        "--model_path", type=str, default="models/", help="Path to save model"
+    )
     args = parser.parse_args()
 
     df_train = pd.read_csv(args.data_path + "train.csv")
@@ -26,7 +38,6 @@ def main():
     df_test = pd.read_csv(args.data_path + "test.csv")
     X_test = df_test.drop("popularity", axis=1)
     y_test = df_test["popularity"]
-
 
     mlflow.set_experiment("Music_Popularity_Prediction")
 
@@ -39,40 +50,41 @@ def main():
             "n_estimators": args.n_estimators,
             "max_depth": args.max_depth,
             "min_samples_split": args.min_samples_split,
-            "random_state": 42
+            "random_state": 42,
         }
-        
+
         model = RandomForestRegressor(**params)
         model.fit(X_train, y_train)
-        
+
         predictions = model.predict(X_test)
         rmse = np.sqrt(mean_squared_error(y_test, predictions))
         r2 = r2_score(y_test, predictions)
-        
+
         mlflow.log_params(params)
         mlflow.log_metric("rmse", rmse)
         mlflow.log_metric("r2_score", r2)
-        
+
         plt.figure(figsize=(10, 8))
         feat_importances = pd.Series(model.feature_importances_, index=X_test.columns)
-        feat_importances.nlargest(15).sort_values().plot(kind='barh', color='skyblue')
+        feat_importances.nlargest(15).sort_values().plot(kind="barh", color="skyblue")
         plt.title("Top 15 Feature Importances")
         plt.xlabel("Importance Score")
         plt.tight_layout()
-        
+
         plot_path = "feature_importance.png"
         plt.savefig(plot_path)
         mlflow.log_artifact(plot_path)
-        
+
         mlflow.sklearn.log_model(model, "random_forest_model")
         os.makedirs(args.model_path, exist_ok=True)
         filename = args.model_path + "random_forest_model.pkl"
-        with open(filename, 'wb') as file:
-            pickle.dump(model, file)          
+        with open(filename, "wb") as file:
+            pickle.dump(model, file)
         metrics = {"rmse": rmse, "r2": r2}
         with open("metrics.json", "w", encoding="utf-8") as f:
-            json.dump(metrics, f, indent=2) 
+            json.dump(metrics, f, indent=2)
         print(f"✅ Run complete! RMSE: {rmse:.4f}, R2: {r2:.4f}")
+
 
 if __name__ == "__main__":
     main()
